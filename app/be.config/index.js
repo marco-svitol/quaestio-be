fdb54b9d2d73a58a68faae4e0c68b30171755b35
Marco Cambon <marco.cambon@webfleet.com>
2023-04-25 17:28:11 +0200

    token timeout increased

--- app/be.config/index.js
+++ app/be.config/index.js
@@ -1,37 +1,37 @@
 // requires
-const _         = require('lodash');
-const dotenv    = require('dotenv');
+const _ = require('lodash');
+const dotenv = require('dotenv');
 
 // module variables 
-const config                        = require('./config/config.json');
-const defaultConfig                 = config.development;
-const environment                   = process.env.NODE_ENV || 'production';
-const environmentConfig             = config[environment];
-const finalConfig                   = _.merge(defaultConfig, environmentConfig);
+const config = require('./config/config.json');
+const defaultConfig = config.development;
+const environment = process.env.NODE_ENV || 'production';
+const environmentConfig = config[environment];
+const finalConfig = _.merge(defaultConfig, environmentConfig);
 dotenv.config();
-finalConfig.app.opsBaseUrl              = process.env.OPSBASEURL;
-finalConfig.app.opsClientID             = process.env.OPSCLIENTID;
-finalConfig.app.opsClientSecret         = process.env.OPSCLIENTSECRET;
-finalConfig.app.certpath                = process.env.CERTPATH;
-finalConfig.app.certpw                  = process.env.CERTPW;
-finalConfig.sqlConfigPool.user      = process.env.SQLCONFIG_DBUSER;
-finalConfig.sqlConfigPool.password  = process.env.SQLCONFIG_DBPW;
-finalConfig.sqlConfigPool.server    = process.env.SQLCONFIG_DBSERVER;
-finalConfig.sqlConfigPool.database  = process.env.SQLCONFIG_DBNAME;
-finalConfig.tokenProperties.secret  = process.env.TOKEN_SECRET;
+finalConfig.app.opsBaseUrl = process.env.OPSBASEURL;
+finalConfig.app.opsClientID = process.env.OPSCLIENTID;
+finalConfig.app.opsClientSecret = process.env.OPSCLIENTSECRET;
+finalConfig.app.certpath = process.env.CERTPATH;
+finalConfig.app.certpw = process.env.CERTPW;
+finalConfig.sqlConfigPool.user = process.env.SQLCONFIG_DBUSER;
+finalConfig.sqlConfigPool.password = process.env.SQLCONFIG_DBPW;
+finalConfig.sqlConfigPool.server = process.env.SQLCONFIG_DBSERVER;
+finalConfig.sqlConfigPool.database = process.env.SQLCONFIG_DBNAME;
+finalConfig.tokenProperties.secret = process.env.TOKEN_SECRET;
 finalConfig.tokenProperties.refresh_secret = process.env.REFRESH_TOKEN_SECRET;
 
-finalConfig.app.loglevel              = process.env.LOGLEVEL;
-finalConfig.app.serverPort              = process.env.SERVERPORT;
+finalConfig.app.loglevel = process.env.LOGLEVEL || finalConfig.app.loglevel;
+finalConfig.app.serverPort = process.env.SERVERPORT || finalConfig.app.serverPort;
 
-finalConfig.app.logdnakey               = process.env.LOGDNAKEY;
-finalConfig.app.loghostname             = process.env.LOGHOSTNAME;
-finalConfig.app.logenv                  = process.env.LOGENV;
-finalConfig.app.loglevel                = process.env.LOGLEVEL;
+finalConfig.app.logdnakey = process.env.LOGDNAKEY;
+finalConfig.app.loghostname = process.env.LOGHOSTNAME;
+finalConfig.app.logenv = process.env.LOGENV;
 
-if (!finalConfig.app.opsBaseUrl) {console.log ("Fatal error: OPSBASEURL missing");};
-if (!finalConfig.app.opsClientID) {console.log ("Fatal error: OPSCLIENTID missing");};
-if (!finalConfig.app.opsClientSecret) {console.log ("Fatal error: OPSCLIENTSECRET missing");};
+if (!finalConfig.app.opsBaseUrl) { console.log("Fatal error: OPSBASEURL missing"); };
+if (!finalConfig.app.opsClientID) { console.log("Fatal error: OPSCLIENTID missing"); };
+if (!finalConfig.app.opsClientSecret) { console.log("Fatal error: OPSCLIENTSECRET missing"); };
 
-module.exports.config               = finalConfig;
-module.exports.environment          = environment;
+
+module.exports.config = finalConfig;
+module.exports.environment = environment;
