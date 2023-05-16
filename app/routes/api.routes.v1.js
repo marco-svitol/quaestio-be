8d33204c62634b7e1e0a9f4c55ca983c2ae898df
Marco <62987024+marco-svitol@users.noreply.github.com>
2023-05-17 01:52:19 +0200

    Feature/images (#48)
    * image links and new api

--- app/routes/api.routes.v1.js
+++ app/routes/api.routes.v1.js
@@ -1,7 +1,6 @@
 module.exports = myapp => {
   var {unless}          = require('express-unless');
   const routerapp       = require("express").Router();
-  //const routerappbeta   = require("express").Router();
   const routermetrics   = require("express").Router();
   const apitest         = require("../logic/v1/api.test");
   const apisearch       = require("../logic/v1/api.search")
@@ -14,23 +13,29 @@ module.exports = myapp => {
 
   cacheMiddleware.unless = unless; 
   routerapp.use(cacheMiddleware.unless({
-    path: ['/api/v1/test','/api/v1/cachereset','/api/v1/auth/login','/api/v1/auth/refresh','/api/v1/userprofile','/api/v1/opstest','/api/v1/search']
+    path: [
+      '/api/v1/cachereset',
+      '/api/v1/test',
+      '/api/v1/opstest',
+      '/api/v1/search',
+      '/api/v1/userprofile',
+      "/api/v1/firstpageClipping",
+      '/api/v1/auth/login',
+      '/api/v1/auth/refresh'
+    ]
   }))
 
   routerapp.post("/cachereset",       apitest.cacheReset);
   routerapp.get("/test",              apitest.test );
   routerapp.get("/opstest",           apitest.opstest );
-  routerapp.get("/fsearch",           apitest.search );
   routerapp.get("/search",            apisearch.search );
-  routerapp.get("/publication",       apisearch.publication );
+  //routerapp.get("/publication",       apisearch.publication );
   routerapp.get("/userprofile",       apisearch.userprofile);
   routerapp.get("/opendoc",           apisearch.opendoc);
+  routerapp.get("/firstpageClipping", apisearch.firstpageClipping);
   routerapp.get("/auth/login",        apiauth.login);
   routerapp.get("/auth/refresh",      apiauth.refresh);
   myapp.use('/api/v1', routerapp);
-  // routerappbeta.get("/search",        apisearch.search );
-  // routerappbeta.get("/publication",       apisearch.publication );
-  // myapp.use('/api/v1/beta', routerappbeta);
 
   routermetrics.get("/health", apihealth.health);
   myapp.use('/', routermetrics);
