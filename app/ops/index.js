9ad96163df865e454e45c9d87f3ee11b47e3cae2
marco <marco.cambon@gmail.com>
2024-01-09 01:13:30 +0100

    typo

--- app/ops/index.js
+++ app/ops/index.js
@@ -290,7 +290,7 @@ module.exports = class opsService{
       body['bibliographic-data']['parties']['applicants']['applicant']
     ) {
       // Ensure that 'applicant' is an array
-      docData.applicant = Array.isArray(body['bibliographic-data']['parties']['applicants']['applicant'])
+      body['bibliographic-data']['parties']['applicants']['applicant'] = Array.isArray(body['bibliographic-data']['parties']['applicants']['applicant'])
       ? body['bibliographic-data']['parties']['applicants']['applicant']
       : [body['bibliographic-data']['parties']['applicants']['applicant']];
       // Filter items based on the condition
